870f42456c1b9f4118dd988abe862daa4ef8ed67
Wiehahn <wiehahne@amazon.com>
2019-12-11 00:39:57 -0600

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import send_file
 app = Flask(__name__)
 @app.route("/summer")
 def get_image():
-    filename = 'cape-town-480x300.jpg'
+    filename = '/cape-town-480x300.jpg'
     return send_file(filename, mimetype='image/jpg')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("80"), debug=True)
