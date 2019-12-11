10bd133d39422734422dbff0109848f41b160d7e
Wiehahn <wiehahne@amazon.com>
2019-12-11 01:04:56 -0600

    s

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 from flask import send_file
 @app.route('/summer')
 def summer():
-    filename = 'uploads\\cape-town-480x300.jpg'
-    return send_file(filename, mimetype='image/jpg')
+    if request.args.get('type') == '1':
+       filename = 'cape-town-480x300.jpg'
+    else:
+       filename = 'cape-town-480x300.jpg'
+    return send_file(filename, mimetype='image/gif')
