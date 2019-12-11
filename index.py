f79463cd5492177b5fe5f088e1f83e49048201bf
Wiehahn <wiehahne@amazon.com>
2019-12-11 00:50:59 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import send_file
-@app.route('/get_image')
-def get_image():
+@app.route('/summer')
+def summer():
     filename = 'uploads\\cape-town-480x300.jpg'
     return send_file(filename, mimetype='image/jpg')
