7bf625d17043246bd9b640bd69532f535d4b53f0
Wiehahn <wiehahne@amazon.com>
2019-12-11 00:50:31 -0600

    d

--- index.py
+++ index.py
@@ -1,8 +1,5 @@
 from flask import send_file
-app = Flask(__name__)
-@app.route("/summer")
-def summer():
-    filename = '/cape-town-480x300.jpg'
+@app.route('/get_image')
+def get_image():
+    filename = 'uploads\\cape-town-480x300.jpg'
     return send_file(filename, mimetype='image/jpg')
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("80"), debug=True)
